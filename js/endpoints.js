23a6e120d5a5339e2a71e6253b86cc2e3ddcbdee
Robin Berjon <robin@berjon.com>
2015-06-03 14:15:05 +0200

    fix URL resolution in endpoints

--- js/endpoints.js
+++ js/endpoints.js
@@ -3,6 +3,6 @@ var url = require("url");
 
 if (!window.config && window.config.api) throw(new Error("You must specify a config.js file."));
 
-exports.user = url.resolve(window.config.api, "/api/user");
-exports.events = url.resolve(window.config.api, "/api/events");
+exports.user = url.resolve(window.config.api, "api/user");
+exports.events = url.resolve(window.config.api, "api/events");
 
