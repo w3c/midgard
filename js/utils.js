3c9d59543d41ee321f5763449156354f529e2fbb
Robin Berjon <robin@berjon.com>
2015-08-13 13:43:08 +0200

    add utils

---
+++ js/utils.js
@@ -0,0 +1,30 @@
+
+import React from "react";
+import MessageActions from "../actions/messages";
+
+
+let config = require("../config.json");
+
+module.exports = {
+    jsonHandler:    (res) => { return res.json(); }
+,   catchHandler:   (e) => {
+        MessageActions.error(e);
+    }
+,   pathPrefix: () => {
+        return config.pathPrefix;
+    }
+,   val:    (ref) => {
+        let el = React.findDOMNode(ref)
+        ,   value
+        ;
+        if (!el) return null;
+        if (el.multiple) {
+            value = [];
+            for (var i = 0, n = el.selectedOptions.length; i < n; i++) {
+                value.push(el.selectedOptions.item(i).value.trim());
+            }
+        }
+        else value = el.value.trim();
+        return value;
+    }
+};
